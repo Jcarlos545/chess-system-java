e86bce524a2498bacef40b739e5041e237a35948
Jcarlos545 <jaocarlos.545@gmail.com>
2019-04-30 09:42:23 -0300

    first class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import bordgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World");
-
+	
+		Position position = new Position(2, 6);
+		
+		System.out.println(position);
 	}
 
 }
